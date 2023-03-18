0946ac4ff4151dc0aef5b0ff557aa2144a7aba0e
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-18 22:24:07 +0100

    Implemented AnecdoteList.

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { voteFor } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return anecdotes
+    .sort((a, b) => b.votes - a.votes)
+    .map((anecdote) => (
+      <div key={anecdote.id}>
+        <div>{anecdote.content}</div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => dispatch(voteFor(anecdote.id))}>vote</button>
+        </div>
+      </div>
+    ));
+};
+
+export default AnecdoteList;
