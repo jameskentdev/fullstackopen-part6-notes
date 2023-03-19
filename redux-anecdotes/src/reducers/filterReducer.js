842f906f7b601453436020970ab20dfc74ec7e3e
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-19 17:39:19 +0100

    Refactored filterReducer to slice.

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,14 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit';
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
       return action.payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-const setFilter = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  };
-};
-
-module.exports = {
-  filterReducer,
-  setFilter,
-};
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
