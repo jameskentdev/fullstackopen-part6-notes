3b844b164f29c735348b365616de7899cc7c5ecc
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-23 19:13:11 +0100

    Added query for retrieving notes.

--- query-notes/src/App.js
+++ query-notes/src/App.js
@@ -1,32 +1,43 @@
+import { useQuery } from 'react-query';
+import { getNotes } from './requests';
+
 const App = () => {
   const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    console.log(content);
+  };
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    console.log('toggle importance of', note.id);
+  };
+
+  const result = useQuery('notes', () => getNotes);
+
+  console.log(result);
+
+  if (result.isLoading) {
+    return <div>Loading data...</div>;
   }
 
-  const notes = []
+  const notes = result.data;
 
-  return(
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
+          {note.content}
           <strong> {note.important ? 'important' : ''}</strong>
         </li>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
